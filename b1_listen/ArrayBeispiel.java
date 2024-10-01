ff56fbc8f02e476c6fe1da5ca44beca9dbe93b75
blolm <125360625+blolm@users.noreply.github.com>
2024-10-01 10:53:14 +0000

    01.10.2024 Array/ArrayList

---
+++ b1_listen/ArrayBeispiel.java
@@ -0,0 +1,16 @@
+package b1_listen;
+
+public class ArrayBeispiel {
+
+    public static void main(String[] args) {
+        
+        double messwerte[] = new double[3];
+        messwerte[0] = 1.3;
+        messwerte[1] = 1.8;
+        messwerte[2] = 1.5;
+        
+        for (double mw : messwerte) {
+            System.out.println(mw);
+        }
+    }
+}
---
+++ b1_listen/ArrayListBeispiel.java
@@ -0,0 +1,47 @@
+package b1_listen;
+
+import java.util.ArrayList;
+
+public class ArrayListBeispiel {
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        //ArrayListe erstellen
+        ArrayList<Integer> primzahl = new ArrayList<>();
+
+        //Objekte hinzufügen
+        primzahl.add(2);
+        primzahl.add(3);
+        primzahl.add(5);
+        primzahl.add(11);
+
+        //Liste ausgeben 
+        System.out.println(primzahl);
+
+        //Objekt entfernen 
+        primzahl.remove(Integer.valueOf(2)); //Entfernt eintrag mit dem Wert 2
+        primzahl.remove(2); //Entfernt eintrag mit dem Index 2
+
+         //Liste ausgeben 
+        System.out.println(primzahl);
+
+        //Über Liste iterieren
+        for (Integer i : primzahl) {
+            System.out.println(i);
+        }
+
+        //prüfen ob Wert in Liste sind 
+        if(primzahl.contains(5)){
+            System.out.println("5 ist in der Liste");
+        } 
+
+        //Wert aus Liste holen
+        Integer i1 = primzahl.get(Integer.valueOf(1));   
+        System.out.println(i1+" an Index 1");
+        
+        System.out.println(primzahl);
+    }
+    
+}
