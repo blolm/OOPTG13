7ae0b29d24744f72434f5fdd03fb98f23fc14780
blolm <125360625+blolm@users.noreply.github.com>
2025-01-08 10:12:11 +0000

    save

---
+++ Ki/KNN Algorithm/Dataobjekt.java
@@ -0,0 +1,27 @@
+
+public class Dataobjekt {
+
+    int x;
+    int y;
+    int klasse = -1;
+
+    
+    public dataobjekt(int x, int y, int k){
+        this.x = x;
+        this.y = y;
+        this.klasse = k;
+    }
+    
+public void setX(int x) {
+    this.x = x;
+}
+public int getX() {
+    return x;
+}
+public void setY(int y) {
+    this.y = y;
+}
+public int getY() {
+    return y;
+}
+}
