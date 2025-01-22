5541fb2d7b6b71a33cfb527a65e9546356795d3f
blolm <125360625+blolm@users.noreply.github.com>
2025-01-22 10:08:58 +0000

    safe

---
+++ Ki/K Means Algorithm/Dataobjekt.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Dataobjekt {
+
+    int x;
+    int y;
+    int myCluster;
+    ArrayList<Integer> Cluster = new ArrayList<>();
+
+    public Dataobjekt(int x, int y, int myCluster){
+        this.x=x;
+        this.y=y;
+        this.myCluster = null;
+
+    }
+}
---
+++ Ki/K Means Algorithm/Zentrum.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+public class Zentrum {
+
+    ArrayList<myCluster> cluster = new ArrayList<>();
+    double delta = 0;
+    do{
+        Set<Double,Cluster> distanzen = new TreeMap<>();
+
+    }while(delta > 0);
+
+}
